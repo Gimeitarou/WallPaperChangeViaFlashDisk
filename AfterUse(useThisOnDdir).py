b623c65d4c11c32e4337fde5f2fb2143bd2f144d
Gimeitarou <300kyen@gmail.com>
2024-11-06 19:48:35 +0900

    Version1.0

---
+++ AfterUse(useThisOnDdir).py
@@ -0,0 +1,9 @@
+import subprocess
+
+f = open('D:\\pic_name.txt', 'r', encoding='UTF-8')
+basename = f.read() #read the note text of the file name of the pic
+f.close()
+
+subprocess.run('attrib -r -h d:\\ChangeWP.exe' #unvail ChangeWP.exe
+                +f'&& attrib -r -h d:\\{basename}' #unvail the pic
+                +'&& attrib -r -h d:\\pic_name.txt', shell=True) #unvail note of file name of the pic
